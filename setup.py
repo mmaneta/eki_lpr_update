c0fd85c61329874714dc5ca311948f0a07996fd5
Marco Maneta <mpmaneta@gmail.com>
2024-12-18 17:03:16 -0800

    fixed == version pinning

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(
     author='Marco Maneta',
     author_email='mmaneta@ekiconsult.com',
     description='',
-    install_requires=["requests", "matplotlib", "jupyter", "pandas", "pyarrow", "numpy", "pypdf=4.2.0",  "fpdf2=2.7.9",  "pillow"]
+    install_requires=["requests", "matplotlib", "jupyter", "pandas", "pyarrow", "numpy", "pypdf==4.2.0",  "fpdf2==2.7.9",  "pillow"]
 )
